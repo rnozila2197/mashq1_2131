aa66c18ef0163dcb3436c0ac7ded0080bf17bee8
rnozila2197 <yjamshid2197@gmail.com>
2025-11-12 18:19:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#8
+matn = "men python organaman"
+i = matn.replace("python", "Java")
+print(i)
+
+#9
+raqam = input("telefon raqam: ")
+i = raqam.replace("-", " ")
+print(i)
+
+#10
+matn = "Salom dunyo"
+print(matn.replace(" ", "-"))
+
+#11
+i = input("Gap kirit: ")
+e = i.find("Salom bu men!")
+print(e)
+
+#12
+matn = input("matn kirit: ")
+q = input("qidirilayotgan soz kirit: ")
+
+paz = matn.find(q)
+print(paz)
